620e44329115c2c42f3aba10ab56ae93ffbd2010
ivan avila <35358557+althios@users.noreply.github.com>
2021-05-20 21:17:54 -0500

    Node_Graph_1.2

--- Nodes_Graph.R
+++ Nodes_Graph.R
@@ -1,14 +1,17 @@
 set.seed(1029)
 h=2.7
-M=rep(c(1,1+h,1+2*h,1+3*h),5)/6
-N=1:20/20
+n=5
+m=10
+k=m*n/2
+M=rep(1+(1:n)*h,m)/(m+1)
+N=1:(n*m)/(n*m)
 plot(M,N,xlim=c(0,2),ylim=c(0,1))
-M1=sample(1:50,replace=T)
-M2=sample(1:50,replace=T)
+M1=sample(1:(m*n),k,replace=T)
+M2=sample(1:(m*n),k,replace=T)
 lines(c(M[M2],M[M1]),c(N[M2],N[M1]),type='l',lty=3,lwd=1,col="red")
-M1=sample(1:50,replace=T)
-M2=sample(1:50,replace=T)
-lines(c(M[M2],M[M1]),c(N[M2],N[M1]),type='l',lty=3,lwd=2,col="blue")
-M1=sample(1:50,replace=T)
-M2=sample(1:50,replace=T)
+M1=sample(1:(m*n),k,replace=T)
+M2=sample(1:(m*n),k,replace=T)
+lines(c(M[M2],M[M1]),c(N[M2],N[M1]),type='l',lty=3,lwd=1.5,col="blue")
+M1=sample(1:(m*n),k,replace=T)
+M2=sample(1:(m*n),k,replace=T)
 lines(c(M[M2],M[M1]),c(N[M2],N[M1]),type='l',lty=3,lwd=0.5,col="dark green")
